cc336ab8da4ae44f42821c30b1c0ef003709233b
VERAPAO <pmnvera1984@gmail.com>
2024-02-12 10:19:45 -0300

    Realizo commit de algunos cambios de dimensiones

--- app.js
+++ app.js
@@ -9,5 +9,12 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'))
 });
 
+app.get('/registro', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'))
+});
+
+app.get('/ingresa', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'))
+});
 
 app.listen(port, () => console.log (`https://localhost:${port}`));
