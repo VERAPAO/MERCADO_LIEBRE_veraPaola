0022525195eaed89daec67485c9efbc95202a93d
VERAPAO <pmnvera1984@gmail.com>
2024-03-08 19:10:54 -0300

    Finalización del todo el CRUD

---
+++ src/controllers/admin/destroy.controller.js
@@ -0,0 +1,14 @@
+
+
+const { loadData, saveData } = require('../../data')
+
+module.exports = (req, res) => {
+
+    const { id } = req.params;
+    const products = loadData();
+    const productsLessOne = products.filter(p => p.id !== +id);
+
+    saveData(productsLessOne);
+
+    res.redirect('/productos')
+}
